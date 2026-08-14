79686c77c1c1cedaaeffccc3f0a7c7ca03403cf2
Pavel Fadeev <pavel.fadeev@gmail.com>
2026-08-14 02:16:51 +0200

    chore: remove retired runtime subsystems

--- packages/core/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./context-broker.js";
-export * from "./feature-status.js";
-export * from "./paths.js";
-export * from "./risk.js";
-export * from "./secure-fs.js";
-export * from "./storage.js";
-export * from "./text.js";
--- packages/orchestration/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { default, registerAutoresearch, registerGoal, registerLoop, registerOrchestration } from "./extension.js";
-export { clearWorker, formatWorkerState, handleWorkerCommand, readWorkerState, workerArgumentCompletions, workerSystemPrompt } from "./worker.js";
-export { classifyWorkerOutcome, clearWorkerRequestTracking, recordWorkerRequest, recordWorkerResult } from "./worker-telemetry.js";
-export { dispatchWorker, resolveConfiguredWorkerModel } from "./worker-dispatch.js";
-export { orchestrationFeatureStatus, serializeOrchestrationFeatureStatus } from "./status.js";
-export type { WorkerOutcome, WorkerTelemetryEvent } from "./worker-telemetry.js";
